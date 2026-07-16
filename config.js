ceafe4e856ad75985674baaa65d4b05b63a568d9
Seu Nome <seu-email@gmail.com>
2026-07-16 14:07:42 -0300

    Corrigir ID da planilha

--- config.js
+++ config.js
@@ -8,8 +8,9 @@
 // ============================================
 
 const LOJAFACIL = {
- planilha_configuracoes: "https://docs.google.com/spreadsheets/d/1jKRrfuBF7AVjzCF5ZYgbRlQW2dQ9_hXY_pWJfRv4lZy/export?format=csv&gid=480609827",
+  planilha_configuracoes: "https://docs.google.com/spreadsheets/d/1jKRrfuBF7AVjzCF5ZYgbRqIW2dQ9_hXY_pWJfRv4IzY/export?format=csv&gid=480609827",
 
-planilha_produtos: "https://docs.google.com/spreadsheets/d/1jKRrfuBF7AVjzCF5ZYgbRlQW2dQ9_hXY_pWJfRv4lZy/export?format=csv&gid=952991100",
+  planilha_produtos: "https://docs.google.com/spreadsheets/d/1jKRrfuBF7AVjzCF5ZYgbRqIW2dQ9_hXY_pWJfRv4IzY/export?format=csv&gid=952991100",
+
   salvar_pedidos: "https://script.google.com/macros/s/AKfycbzL5nsc-NidM_ZIGAOjmp8-7z4eSMjf8p39CBnCasj_tzlQ/exec"
 };
