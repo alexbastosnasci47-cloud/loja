33b10b918ffcbc62db9838ebe0b6404cb01e19fd
Seu Nome <seu-email@gmail.com>
2026-07-16 23:00:14 -0300

    Usar links publicados da planilha

--- config.js
+++ config.js
@@ -8,9 +8,9 @@
 // ============================================
 
 const LOJAFACIL = {
-  planilha_configuracoes: "https://docs.google.com/spreadsheets/d/1jKRrfuBF7AVjzCF5ZYgbRqIW2d9Q_hXY_pWJfRv4IzY/export?format=csv&gid=480609827",
+  planilha_configuracoes: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRMQmn7dbdO-lIFtYhSjehP0moGcKipeoJ_4rTg90JzSFtE3x8xUv7tZcObwVwzWIldxCQankWR4juv/pub?output=csv&gid=480609827",
 
-planilha_produtos: "https://docs.google.com/spreadsheets/d/1jKRrfuBF7AVjzCF5ZYgbRqIW2d9Q_hXY_pWJfRv4IzY/export?format=csv&gid=952991100",
+  planilha_produtos: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRMQmn7dbdO-lIFtYhSjehP0moGcKipeoJ_4rTg90JzSFtE3x8xUv7tZcObwVwzWIldxCQankWR4juv/pub?output=csv&gid=952991100",
 
   salvar_pedidos: "https://script.google.com/macros/s/AKfycbzL5nsc-NidM_ZIGAOjmp8-7z4eSMjf8p39CBnCasj_tzlQ/exec"
 };
